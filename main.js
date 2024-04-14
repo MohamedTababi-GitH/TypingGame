131daa735a0446f3f8e352b0fcbfe0eee7ae3b2a
Mohamed Tababi <tababi.md@gmail.com>
2024-04-14 21:52:00 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+const randomWords = ["zebra", "umbrella", "jazz", "kangaroo", "xylophone", "quasar", "flamingo", "volcano", "quokka", "ninja", "whiskey", "xylophone", "zombie", "penguin", "ostrich", "jigsaw", "koala", "whale", "unicorn", "rhubarb", "sphinx", "chocolate", "muffin", "tornado", "robot", "penguin", "galaxy", "parachute", "helicopter", "quicksilver", "butterfly", "dinosaur", "lightning", "hurricane", "bicycle", "narwhal", "octopus", "dragon", "snail", "meteor", "whirlwind", "goblin", "jellyfish", "toothpaste", "giraffe", "fireworks", "rainbow", "lollipop", "moonwalk"];
+let randomNumber = Math.floor(Math.random() * randomWords.length) + 1;
+const button = document.getElementById("btn");
+const label = document.getElementById("txt");
+const input = document.getElementById("input");
+const para = document.createElement("p");
+input.focus();
+label.textContent = randomWords[randomNumber];
+let labelValue = label.textContent;
+let inputValue = input.value;
+
+console.log("label text: " + labelValue);
+console.log("input tex: " + inputValue);
+
+button.addEventListener("click", CompareText);
+input.addEventListener('keypress', function (e) {
+  if (e.key === 'Enter') {
+    CompareText();
+  }
+});
+
+function CompareText() {
+  inputValue = input.value;
+    console.log("label text: " + labelValue);
+    console.log("input tex: " + inputValue);
+  if (labelValue === inputValue){
+  para.textContent = "Correct!";
+  input.value = "";
+} else {
+  para.textContent = "Incorrect"; 
+  input.value = "";
+}
+randomNumber = Math.floor(Math.random() * randomWords.length - 1) + 1;
+console.log("random number: " + randomNumber);
+  label.textContent = randomWords[randomNumber];
+  labelValue = label.textContent;
+    
+document.body.appendChild(para);}
+});
